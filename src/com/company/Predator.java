0163b69bd9fbd2fe6672e0ac987ba743ea369b91
NeAlexei <a.nechitaylo@hypervsn.com>
2019-01-02 14:13:26 +0300

    02.01.19 Commit 14:13

--- src/com/company/Predator.java
+++ src/com/company/Predator.java
@@ -5,6 +5,11 @@ public class Predator extends Animal{
             if (meal instanceof Grass){ //проверка: если еда есть трава - сообщение, если еда есть мясо - норм
                 System.out.println("Хищник не ест траву!");
             } else {
+                /*if (meal.getWeight >= weight){
+                    System.out.printLn("Вес хищника меньше веса "жертвы", он не может это съесть);
+                 } else {
+                    weight = weight + meal.getWeight(); */
+
                 weight = weight + meal.getWeight();
             }
             return weight;

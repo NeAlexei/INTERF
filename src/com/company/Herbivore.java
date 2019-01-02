4528bfdfdd2d54ee6f6a999f022602581f9f16d8
NAlexei <nechytaila89@gmail.com>
2019-01-03 01:17:40 +0300

    commit4

--- src/com/company/Herbivore.java
+++ src/com/company/Herbivore.java
@@ -3,11 +3,12 @@ package com.company;
 public class Herbivore extends Animal {
 
     public int toEat(Meal meal){
-        if (meal instanceof Meat){ // Проверка, является ли тип объекта meal типом Meat
-            System.out.println("Травоядное животное не ест мясо!");
-        } else {
+//        if (meal instanceof Meat){ // Проверка, является ли тип объекта meal типом Meat
+//            System.out.println("Травоядное животное не ест мясо!");
+//        } else {
             weight = weight + meal.getWeight();
-        }
+//        }
         return weight;
     }
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,21 +4,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-    Trava trava = new Trava(4);
-    Meal ukrop = trava;
+    Herbivore trava = new Trava(5);
+        System.out.println("Трава весит = " + ((Trava) trava).weight);
+        Meal ukrop;
+        ukrop = trava;
+        System.out.println("Укроп весит = " + trava.weight);
 
-    Squirrel squirrel = new Squirrel(6);
+
+    Herbivore squirrel = new Squirrel(6);
+        System.out.println("Вес белки = " );
     Animal animal_01 = squirrel;
-        System.out.println("Белка съела траву и стала весить: " + animal_01.toEat(ukrop));
+        System.out.println("Белка съела траву и стала весить: " + animal_01.toEat(trava));
 
-    Rabbit rabbit = new Rabbit(8);
-    Animal animal_02 = rabbit;
-        System.out.println("Кролик съел траву и стал весить: " + animal_02.toEat(ukrop));
-
-    Wolf wolf = new Wolf(9);
-    Animal animal_03 = wolf;
-        System.out.println("Волк съел голодную белочку и стал весить: " + animal_03.toEat(squirrel));
-        System.out.println("Волк не съел сытую белочку: " + animal_03.toEat(animal_01));
+//    Rabbit rabbit = new Rabbit(8);
+//    Animal animal_02 = rabbit;
+//        System.out.println("Кролик съел траву и стал весить: " + animal_02.toEat(ukrop));
+//
+//    Wolf wolf = new Wolf(9);
+//    Animal animal_03 = wolf;
+//        System.out.println("Волк съел голодную белочку и стал весить: " + animal_03.toEat(squirrel));
+//        System.out.println("Волк не съел сытую белочку: " + animal_03.toEat(animal_01));
 
 
 
